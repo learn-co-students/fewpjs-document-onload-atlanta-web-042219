7173b39b84fca94b47f58f0a8a1988c2d353e2d1
github username <github email address>
2019-06-07 09:19:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector('#text').innerHTML = "This is really cool!"
+  document.getElementById('text').innerHTML = "This is really cool!"
 })
 
